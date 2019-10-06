41516db1290af3d193b89eb1a88f6a45cefdea63
mmonahan <dr.mirkops@gmail.com>
2019-10-06 19:22:23 +0300

    print key yes

--- main.c
+++ main.c
@@ -6,12 +6,22 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 19:12:41 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/10/05 20:18:02 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/10/06 19:21:52 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "minilibx_macos/mlx.h"
+
+# define GORIZONT 1000
+# define VERTICAL 1000
+
+int dd(int key, void *param)
+{
+	printf("%d\n", key);
+	//mix_pixet_put(mlx_ptr, win_ptr, );
+	return (0);
+}
 
 int main()
 {
@@ -20,8 +30,11 @@ int main()
     printf("Hello, fdf!\n");
 
     mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "mlx 21");
-    mlx_pixel_put(mlx_ptr, win_ptr, 1000/2, 1000/2, 0xFFFFFF);
+    win_ptr = mlx_new_window(mlx_ptr, GORIZONT, VERTICAL, "mlx 21");
+    mlx_pixel_put(mlx_ptr, win_ptr, GORIZONT/2, VERTICAL/2, 0xFFFFFF);
+
+	mlx_key_hook(win_ptr, dd, (void *)0);
+
     mlx_loop(mlx_ptr);
     return 0;
 }
