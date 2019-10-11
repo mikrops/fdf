5089c4cc76d57f7a0cd3a96d1258073d0a0e80d9
mmonahan <dr.mirkops@gmail.com>
2019-10-11 21:03:51 +0300

    lists off, start string!!!

--- input_map.c
+++ input_map.c
@@ -6,7 +6,7 @@
 /*   By: mmonahan <mmonahan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 12:06:20 by mmonahan          #+#    #+#             */
-/*   Updated: 2019/10/11 18:56:39 by mmonahan         ###   ########.fr       */
+/*   Updated: 2019/10/11 21:02:27 by mmonahan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,72 +41,109 @@ int count_digit(char *str)
 	return (count);
 }
 
-void pr(t_list *elem)
-{
-	printf("list[%zu] - %p\n", elem->content_size, elem->content);
-}
+//void pr(t_list *elem)
+//{
+//	printf("list[%zu] - %p\n", elem->content_size, elem->content);
+//}
 
-void add_point(t_list *list, char *str, int size)
+//void add_point(t_list *list, char *str, int size)
+//{
+//	int		i;
+//	int		digit;
+//	t_point	point;
+//	if (list)
+//		;
+//
+//	i = 0;
+//	digit = 0;
+//	while (size)
+//	{
+//		if (ft_isdigit(str[i]))
+//		{
+//			digit = ft_atoi(&str[i]);
+//			i += ft_intcount(digit);
+//			point.z = digit;
+//			//ft_lstadd(&list, ft_lstnew(&point, size > 1 ? 0 : 1));
+//			//printf("addlist[%d] - %p - %zu\n", size, list->content, list->content_size);
+//			size--;
+//		}
+//		else
+//			i++;
+//	}
+//	//ft_lstiter(list, pr);
+//}
+
+void	fill_map(char *map, int y, int x)
 {
-	int		i;
-	int		digit;
-	t_point	point;
+	int i;
+	int j;
+	int digit;
+	int **arr;
 
 	i = 0;
+	j = 0;
 	digit = 0;
-	while (size)
+	arr = ft_map_int(y, x);
+	while(j < y)
 	{
-		if (ft_isdigit(str[i]))
+		while (i < x)
 		{
-			digit = ft_atoi(&str[i]);
-			i += ft_intcount(digit);
-			point.z = digit;
-			ft_lstadd(&list, ft_lstnew(&point, size > 1 ? 0 : 1));
-			//printf("addlist[%d] - %p - %zu\n", size, list->content, list->content_size);
-			size--;
+			if (ft_isdigit(*map))
+			{
+				digit = ft_atoi(map);
+				map += ft_intcount(digit);
+			}
+			else
+				i++;
 		}
-		else
-			i++;
+		i = 0;
+		j++;
 	}
-	ft_lstiter(list, pr);
+	ft_put_map_int_fd(arr, y, x, 1);
 }
 
 int	input_map(char *av)
 {
-	t_list 	*list;
-	t_list	*start;
+//	t_list 	*list;
+//	t_list	*start;
 	int		fd;
 	char	*str;
+	char 	*tmp;
 	int 	i;
 	int 	j;
 
 	fd = open(av, O_RDONLY);
 	i = 0;
 	j = 0;
-	list = ft_memalloc(sizeof(t_list));
-	start = ft_memalloc(sizeof(t_list));
-
-	start = list;
+	tmp = ft_memalloc(1);
+//	list = ft_memalloc(sizeof(t_list));
+//	start = ft_memalloc(sizeof(t_list));
+//
+//	start = list;
 	printf("open(fd = %d)\n", fd);
 	while (get_next_line(fd, &str))
 	{
 		i = count_digit(str);
-		add_point(list, str, i);
-		if (j == 0)
-		{
-			start = list;
-			printf("SAVEEEEEE\n");
-		}
+		//add_point(list, str, i);
+//		if (j == 0)
+//		{
+//			start = list;
+//			printf("SAVEEEEEE\n");
+//		}
+		tmp = ft_strjoin(tmp, str);			//!!!!!исправить ft_strjoin!!!!!!!!
+		free(str);
+		tmp = ft_strjoin(tmp, "\n");	//!!!!!исправить ft_strjoin!!!!!!!!
 		printf("i = %d \t %s\n", i, str);
 		j++;
 	}
-	printf("close(fd = %d) %d %d\n", close(fd), i, j);
+	printf("close(fd = %d) j = %d i = %d\n", close(fd), j, i);
+	printf("%sstring end\n", tmp);
+	/*
+	 * ИСПРАВИТЬ ФИЛЛ!!!
+	*/
+	fill_map(tmp, j, i);
 
-	ft_lstiter(start, pr);
-
-//	printf("%zu\n", list->content_size);
-//	printf("%zu\n", list->next->content_size);
-//	printf("%zu\n", list->next->next->content_size);
+	//ft_lstiter(start, pr);
 	return (0);
 }
 
@@ -119,4 +156,5 @@ int	input_map(char *av)
 **	отсутствие		|	-4
 **					|
 **	----------------------------------
+**
 */
